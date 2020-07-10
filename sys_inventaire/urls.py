93a795e59e8f641d6e2c12fd5a8d612312d3398f
Dje Dorgeles <dgcdorgeles@outlook.fr>
2020-07-10 22:00:43 +0000

    end of the inventory part

--- sys_inventaire/urls.py
+++ sys_inventaire/urls.py
@@ -5,5 +5,8 @@ app_name = 'sys_inventaire'
 
 urlpatterns = [
     path('', index, name='index'), 
-    path('medicaments/', montrer_medicament, name='medicament')
+    path('medicaments/', montrer_medicament, name='medicament'),
+    path('paraphamacies/', montrer_parapharmacie, name='paraphamacies'),
+    path('dietetiques/', montrer_dietetique, name='dietetiques'),
+    path('pti_materiel/', montrer_petit_materiel, name='pti_materiel')
 ]
--- sys_inventaire/views.py
+++ sys_inventaire/views.py
@@ -7,7 +7,30 @@ def index(request):
 
 def montrer_medicament(request):
     items = Medicament.objects.all()
+    header = 'Medicament'
     context = {
-        'items': items,
+        'header':header,
+        'items': items
+    }
+    return render(request, 'inventory.html', context)
+
+def montrer_parapharmacie(request):
+    items = Parapharmatie.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'inventory.html', context)
+
+def montrer_petit_materiel(request):
+    items = Petit_matériel.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'inventory.html', context)
+
+def montrer_dietetique(request):
+    items = Dietetique.objects.all()
+    context = {
+        'items': items
     }
     return render(request, 'inventory.html', context)
