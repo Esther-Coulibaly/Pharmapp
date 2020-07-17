101b4d1bf49e4bfb72133409c2ac94b1e2de8da8
Dje Dorgeles <dgcdorgeles@outlook.fr>
2020-07-17 23:20:51 +0000

    adding the edit btn and fixing the previous bug

--- sys_inventaire/urls.py
+++ sys_inventaire/urls.py
@@ -9,9 +9,16 @@ urlpatterns = [
     path('paraphamacies/', montrer_parapharmacie, name='paraphamacies'),
     path('dietetiques/', montrer_dietetique, name='dietetiques'),
     path('pti_materiel/', montrer_petit_materiel, name='pti_materiel'), 
+
     # les ajouts de nouveaux produits
     path('ajoutMedicament/', ajout_medicament, name='ajouter_medicament'),
     path('ajoutpara/', ajout_parapharmacie, name='ajouter_parapharmacie'),
-    path('ajoutPetitMaretiel/', ajout_petit_materiel, name='ajouter_petit_materiel'),
+    path('ajoutPetitMateriel/', ajout_petit_materiel, name='ajouter_petit_materiel'),
     path('ajoutDietetique/', ajout_dietetique, name='ajouter_dietetique'),
+
+    # La modification particuliere d'articles enregistré en cas d'erreur
+    path('modifierMedicament/<int:pk>/', modifier_medicament, name='modifier_medicament'),
+    path('modifierpar/<int:pk>', modifier_parapharmacie, name='modifier_parapharmacie'),
+    path('modifierpetitmateriel/<int:pk>/', modifier_petit_materiel, name='modifier_petit_materiel'),
+    path('modifierdietetique/<int:pk>/', modifier_dietetique, name='modifier_dietetique'),
 ]
--- sys_inventaire/views.py
+++ sys_inventaire/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
 from .models import *
 from .forms import *
 
@@ -18,21 +19,24 @@ def montrer_medicament(request):
 def montrer_parapharmacie(request):
     items = Parapharmatie.objects.all()
     context = {
-        'items': items
+        'items': items,
+        'titre': 'Parapharmacie'
     }
     return render(request, 'inventory.html', context)
 
 def montrer_petit_materiel(request):
     items = Petit_matériel.objects.all()
     context = {
-        'items': items
+        'items': items,
+        'titre': 'Petit matériel'
     }
     return render(request, 'inventory.html', context)
 
 def montrer_dietetique(request):
     items = Dietetique.objects.all()
     context = {
-        'items': items
+        'items': items,
+        'titre': 'Diétitique'
     }
     return render(request, 'inventory.html', context)
 
@@ -42,7 +46,7 @@ def ajouter_produit(request, cls):
 
         if form.is_valid():
             form.save()
-            redirect('sys_inventaire:index')
+            return redirect('sys_inventaire:index')
     else:
         form = cls()
         return render(request, 'ajout_produit.html', {'form': form})
@@ -58,3 +62,29 @@ def ajout_parapharmacie(request):
 
 def ajout_dietetique(request):
     return ajouter_produit(request, DietetiqueForm)
+
+def modifier_article(request, pk, model, cls):
+    item = get_object_or_404(model, pk=pk)
+    
+    if request.method == 'POST':
+        form = cls(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('sys_inventaire:index')
+    else:
+        form = cls(instance=item)
+        return render(request, 'modifpage.html', {'form':form})
+
+def modifier_medicament(request, pk):
+    return modifier_article(request, pk, Medicament, MedicamentForm)
+
+
+def modifier_dietetique(request, pk):
+    return modifier_article(request, pk, Dietetique, DietetiqueForm)
+
+def modifier_parapharmacie(request, pk):
+    return modifier_article(request, pk, Parapharmatie, ParapharmatieForm)
+
+
+def modifier_petit_materiel(request, pk):
+    return modifier_article(request, pk, Petit_matériel, Petit_matérielForm)
